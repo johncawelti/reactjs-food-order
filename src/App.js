56eaf1fa213b71054952b8d69927f9d227dbfe91
johncawelti <john.cawelti@us.idemia.com>
2023-04-21 09:33:02 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import CartProvider from './store/CartProvider';
 import Cart from './components/Cart';
+// this is a comment
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
